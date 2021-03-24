bb7cb72e644fc5ee9b885d57d825a2c9163014ec
Shane <shane.earley10@gmail.com>
2021-03-24 15:11:48 -0400

    Add token expiration and refresh handling

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
-import { showGreeting } from './lib/welcome.js';
-import { requestSelectAccount } from './lib/api.js';
 import { getClientCredentials } from './lib/config.js';
 import { getMenuSelection } from './lib/menu.js';
-import { setUpCredentials } from './lib/setup.js';
+import { setUpClient } from './lib/setup.js';
 
 const runCli = async () => {
-  const { clientId, clientSecret, accessToken } = await getClientCredentials();
-  const clientNeedsSetup = !clientId || !clientSecret || !accessToken;
-  if (clientNeedsSetup) {
-    await showGreeting();
-    await setUpCredentials();
-    return requestSelectAccount();
-  } else {
-    await getMenuSelection();
+  try {
+    const { clientId, clientSecret, accessToken, refreshToken } = await getClientCredentials();
+    const clientNeedsSetup = !clientId || !clientSecret || !accessToken || !refreshToken;
+    if (clientNeedsSetup) {
+      await setUpClient();
+    } else {
+      await getMenuSelection();
+    }
+  } catch (err) {
+    console.log(err.message)
   }
 };
 
