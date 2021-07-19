a1598ce53030e271be7a6d24bbb54f7e77e65243
shanejearley <shane.earley10@gmail.com>
2021-07-19 10:28:32 -0400

    Remove unnecessary awaits

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ import { setUpClient } from './lib/setup.js';
 
 const runCli = async () => {
   try {
-    const { clientId, clientSecret, accessToken, refreshToken } = await getClientCredentials();
+    const { clientId, clientSecret, accessToken, refreshToken } = getClientCredentials();
     const clientNeedsSetup = !clientId || !clientSecret || !accessToken || !refreshToken;
     if (clientNeedsSetup) {
       await setUpClient();
+      process.exit(1)
     } else {
       await getMenuSelection();
+      process.exit(1)
     }
   } catch (err) {
     console.log(err.message)
+    process.exit(1)
   }
 };
 
